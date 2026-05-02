1b9a833706ed87738ae7bfed597025efed8f5179
bautibatu28 <bautistapanero28@gmail.com>
2026-05-02 17:26:03 -0300

    Commit

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Guía JavaScript iniciada");
+const nombre = "Luna";
+let edad = 18;
+let vida = 100;
+let puntaje = 0;
+let estaActivo = true;
+
+console.log(nombre, edad, vida, puntaje, estaActivo);
+let puntosBase = 100;
+let bonus = 50;
+let penalizacion = 20;
+
+let puntajeTotal = puntosBase + bonus - penalizacion;
+console.log("Puntaje total:", puntajeTotal);
