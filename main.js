fc3da3e023ba7eed59893be8ea6b7cf518250b14
bautibatu28 <bautistapanero28@gmail.com>
2026-05-03 10:28:25 -0300

    commit

--- main.js
+++ main.js
@@ -12,10 +12,23 @@ let penalizacion = 20;
 
 let puntajeTotal = puntosBase + bonus - penalizacion;
 console.log("Puntaje total:", puntajeTotal);
-let edad = 12; 
+edad = 12; 
 
 if (edad >= 13) {
   console.log("Puede jugar");
 } else {
   console.log("No puede jugar todavía");
+}
+vida = 50;
+
+if (vida > 70) {
+  console.log("Jugador en buen estado");
+} else if (vida >= 30) {
+  console.log("Jugador herido");
+} else if (vida >= 1) {
+  console.log("Jugador en peligro");
+} else if (vida === 0) {
+  console.log("Game Over");
+} else {
+  console.log("Error: vida inválida");
 }
